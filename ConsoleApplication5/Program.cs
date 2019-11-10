9405386dbc1f25fb3a5cd8dba47c7168043377d7
Albani28 <albanhysaj28@gmail.com>
2019-11-10 12:02:19 -0800

    first commit

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApplication5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i < 50; i++)
+            {
+                int count = 0; // to count
+                {
+                    
+                }
+            }
+        }
+    }
+}
