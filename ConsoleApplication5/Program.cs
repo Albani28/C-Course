960485d6a8db59cbadfc93423b79b8b0803a84c7
Albani28 <albanhysaj28@gmail.com>
2019-11-10 12:17:04 -0800

    Completed the program

--- ConsoleApplication5/Program.cs
+++ ConsoleApplication5/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleApplication5
+﻿using System;
+
+namespace ConsoleApplication5
 {
     internal class Program
     {
@@ -7,8 +9,20 @@
             for (int i = 1; i < 50; i++)
             {
                 int count = 0; // to count
+                for (int j = i; j<=i; j++
                 {
-                    
+                    if (i % j == 0)
+                    {
+                        count++;
+                        
+                    }
+                }
+
+                {
+                    if (count == 2) // nqs ka 2 plotepjesetues eshte nr i plote
+                    {
+                        Console.WriteLine(i);
+                    }
                 }
             }
         }
