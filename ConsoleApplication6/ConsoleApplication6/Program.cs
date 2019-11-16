e4a43c625335aa60cb15fdf312806333dd5f9862
Albani28 <albanhysaj28@gmail.com>
2019-11-16 12:11:50 -0800

    Added CP6

---
+++ ConsoleApplication6/ConsoleApplication6/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace System
+{
+    class SwitchCase
+    {
+        public static void Main(string[] args)
+        {
+            {
+                char c;
+                int a;
+                int b;
+                Console.WriteLine("Vendos vleren e numrit te pare");
+                a=Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Vendos vleren e numrit te dyte");
+                b=Int32.Parse(Console.ReadLine());
+ 
+                Console.WriteLine("Vendos veprimin");
+                c = Convert.ToChar(Console.ReadLine());
+
+                switch (c)
+                {
+                    case '+':
+                        Console.WriteLine(a+b);
+                        break;
+                    case '/':
+                        Console.WriteLine(a/b);
+                        break;
+                    case '-':
+                        Console.WriteLine(a-b);
+                        break;
+                    case '*':
+                        Console.WriteLine(a*b);
+                        break;
+                    default:
+                        Console.WriteLine("Veprimi qe keni vendosur eshte i pasakte");
+                        break;
+                }
+            }
+        }
+    }
+}
